4fc160c148ff324fa5fd8c87b229b03c9bb415b3
Rama Venkataraman <46733091+Ramav245@users.noreply.github.com>
2022-05-21 15:20:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function compute()
+{
+
+
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if(interest > 0)
+    {
+
+
+    document.getElementById("result").innerHTML="If you deposit " + principal + 
+    " at an interest rate of " + rate + " you will recieve an amount of " + interest + 
+    " in the year " + year; 
+    }
+    else
+    {
+        alert("Enter a positive number.")
+    }
+
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rates").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
